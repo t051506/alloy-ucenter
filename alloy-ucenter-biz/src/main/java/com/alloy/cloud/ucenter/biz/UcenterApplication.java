ad857f104a35b38821accdca52e13ec52469f004
tankechao <tankechao@quanyou.com>
2020-10-07 16:57:02 +0800

    [FIX]根据用户名查询用户

---
+++ alloy-ucenter-biz/src/main/java/com/alloy/cloud/ucenter/biz/UcenterApplication.java
@@ -0,0 +1,19 @@
+package com.alloy.cloud.ucenter.biz;
+
+import com.alloy.cloud.common.security.annotation.EnableCloudFeignClients;
+import com.alloy.cloud.common.security.annotation.EnableCloudResourceServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @Author: tankechao
+ * @Date: 2020/10/7 14:58
+ */
+@SpringCloudApplication
+@EnableCloudResourceServer
+@EnableCloudFeignClients
+public class UcenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UcenterApplication.class, args);
+    }
+}
