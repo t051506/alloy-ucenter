d400b5936ea3c10c9b5a1787b9801fd120ed2f8d
tankechao <tankechao@quanyou.com>
2020-10-17 16:23:32 +0800

    [FIX]用户新增

--- alloy-ucenter-biz/src/main/java/com/alloy/cloud/ucenter/biz/config/SnowflakeConfig.java
+++ alloy-ucenter-biz/src/main/java/com/alloy/cloud/ucenter/biz/config/SnowflakeConfig.java
@@ -1,6 +1,7 @@
 package com.alloy.cloud.ucenter.biz.config;
 
 import com.alloy.cloud.common.core.util.SpringContextUtils;
+import com.alloy.cloud.ucenter.biz.constants.UcenterCache;
 import com.alloy.cloud.ucenter.biz.snowflake.SnowflakeIdWorker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,8 +26,8 @@ public class SnowflakeConfig {
             synchronized (SnowflakeIdWorker.class) {
                 if (instance == null) {
                     StringRedisTemplate stringRedisTemplate = SpringContextUtils.getBean(StringRedisTemplate.class);
-                    Long workerId = stringRedisTemplate.opsForValue().increment("workerId:"+serverName, 1); //不存在则返回1，存在则+1并返回
-                    Long dataCenterId = stringRedisTemplate.opsForValue().increment("dataCenterId:"+serverName, 1);
+                    Long workerId = stringRedisTemplate.opsForValue().increment(UcenterCache.PROJECT_CACHE + "workerId:" + serverName, 1); //不存在则返回1，存在则+1并返回
+                    Long dataCenterId = stringRedisTemplate.opsForValue().increment(UcenterCache.PROJECT_CACHE + "dataCenterId:" + serverName, 1);
                     instance = new SnowflakeIdWorker(workerId % 32, dataCenterId % 32);
                 }
             }
--- alloy-ucenter-biz/src/main/java/com/alloy/cloud/ucenter/biz/service/SysUserService.java
+++ alloy-ucenter-biz/src/main/java/com/alloy/cloud/ucenter/biz/service/SysUserService.java
@@ -6,6 +6,7 @@ import com.alloy.cloud.ucenter.biz.entity.SysUser;
 import com.alloy.cloud.ucenter.biz.mapper.SysUserMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -29,13 +30,19 @@ public class SysUserService{
 
     private final IdGenService idGenService;
 
+    /**
+     * 默认密码: 123456,登陆加密值：LFEt60GoMTj5/mQQ7HjJeA==
+     *    如： http://127.0.0.1:9999/auth/oauth/token?username=test1&password=LFEt60GoMTj5/mQQ7HjJeA==&grant_type=password&scope=server
+     */
     @Transactional(rollbackFor = Exception.class)
     public void insert(SysUser sysUser) {
         sysUser.setUserId(idGenService.genId());
         sysUser.setCreateTime(LocalDateTime.now());
         sysUser.setIsDelete(CommonConstants.STATUS_NORMAL);
-        sysUser.setPassword(ENCODER.encode(sysUser.getPassword()));
+        sysUser.setPassword(ENCODER.encode(StringUtils.isEmpty(sysUser.getPassword()) ? "123456" : sysUser.getPassword()));
         sysUser.setCreateBy(SecurityUtils.getUser().getUsername());
+        sysUser.setIsLock(0);
+        sysUser.setOrgCode(100);
         sysUserMapper.insert(sysUser);
     }
 
