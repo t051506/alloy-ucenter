a2311066585e7203cf627a1625ec151dbdb524b3
tankechao <tankechao@quanyou.com>
2020-11-28 10:55:03 +0800

    [ADD]update sysuser

--- alloy-ucenter-biz/src/main/java/com/alloy/cloud/ucenter/biz/entity/SysUser.java
+++ alloy-ucenter-biz/src/main/java/com/alloy/cloud/ucenter/biz/entity/SysUser.java
@@ -1,16 +1,17 @@
 package com.alloy.cloud.ucenter.biz.entity;
 
-import lombok.Data;
-
-import java.time.LocalDateTime;
+import com.alloy.cloud.common.core.base.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * 用户表
  * @author tankechao
  * @since 2020-10-07 13:26:07
  */
-@Data
-public class SysUser {
+@Getter
+@Setter
+public class SysUser extends BaseEntity {
     private static final long serialVersionUID = 363154537929627918L;
 
     private Long userId;//主键ID
@@ -20,10 +21,6 @@ public class SysUser {
     private String phone;//简介
     private String headImg;//头像
     private Integer orgCode;//组织编码
-    private String createBy;
-    private LocalDateTime createTime;
-    private String updateBy;
-    private LocalDateTime updateTime;//修改时间
     private Integer isLock;//0-正常，9-锁定
-    private Integer isDelete;//0-正常，1-删除
+
 }
