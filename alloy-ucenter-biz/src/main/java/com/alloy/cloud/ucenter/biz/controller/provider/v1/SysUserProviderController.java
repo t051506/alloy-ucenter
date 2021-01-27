06bdc3906f55b54770c0149b1ac9e61f7486f90e
tankechao <t>
2021-01-27 15:31:03 +0800

    [IMP]日志调整

--- alloy-ucenter-biz/src/main/java/com/alloy/cloud/ucenter/biz/controller/provider/v1/SysUserProviderController.java
+++ alloy-ucenter-biz/src/main/java/com/alloy/cloud/ucenter/biz/controller/provider/v1/SysUserProviderController.java
@@ -7,6 +7,7 @@ import com.alloy.cloud.ucenter.api.dto.RemoteUser;
 import com.alloy.cloud.ucenter.biz.entity.SysUser;
 import com.alloy.cloud.ucenter.biz.service.SysUserService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
  * @Author: tankechao
  * @Date: 2020/10/7 14:08
  */
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/provider/v1/user")
@@ -22,13 +24,13 @@ public class SysUserProviderController {
     private final SysUserService sysUserService;
 
     @GetMapping("/info/{username}")
-    public R<RemoteUser> loadByUserName(@RequestHeader(SecurityConstants.FROM) String from, @PathVariable("username") String username){
+    public R<RemoteUser> loadByUserName(@RequestHeader(SecurityConstants.FROM) String from, @PathVariable("username") String username) {
         RemoteUser remoteUser = new RemoteUser();
         SysUser sysUser = sysUserService.queryByUserName(username);
-        if(ObjectUtil.isNull(sysUser)){
+        if (ObjectUtil.isNull(sysUser)) {
             return R.failed("用户不存在");
         }
-        BeanUtils.copyProperties(sysUser,remoteUser);
+        BeanUtils.copyProperties(sysUser, remoteUser);
         return R.ok(remoteUser);
     }
 }
